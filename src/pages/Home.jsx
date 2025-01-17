363ad1b57f6aaddca4d708c08fab4854eb037f97
techmentors2024 <techmentors2024@gmail.com>
2025-01-17 13:46:56 +0700

    Test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -34,19 +34,24 @@ export default function Home() {
                 <ButtonIcon onClick={onIncreasement} icon={<FaPlus />} />
                 <ButtonIcon onClick={onDecreasement} icon={<FaMinus />} />
             </div>
-            <hr />
-            <ul className="grid gap-x-8 gap-y-10 mt-16 sm:grid-cols-2 lg:grid-cols-3">
+            <ul className="px-4 grid gap-x-8 gap-y-10 mt-4 sm:grid-cols-2 lg:grid-cols-3">
                 {
                     data.results && data.results.map((movie) => (
                         <li className="w-full mx-auto group sm:max-w-sm">
                             <a href="#">
-                                <img src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`} loading="lazy" alt={movie.original_title} className="w-full rounded-lg" />
+                                <img src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+                                    loading="lazy"
+                                    alt={movie.original_title} className="w-full rounded-lg" />
                                 <div className="mt-3 space-y-2">
-                                    <span className="block text-indigo-600 text-sm">{movie.release_date}</span>
+                                    <span className="block text-indigo-600 text-sm">
+                                        {movie.release_date}
+                                    </span>
                                     <h3 className="text-lg text-gray-800 duration-150 group-hover:text-indigo-600 font-semibold">
                                         {movie.original_title}
                                     </h3>
-                                    <p className="text-gray-600 text-sm duration-150 group-hover:text-gray-800">{movie.overview}</p>
+                                    <p className="text-gray-600 text-sm duration-150 group-hover:text-gray-800">
+                                        {movie.overview}
+                                    </p>
                                 </div>
                             </a>
                         </li>
